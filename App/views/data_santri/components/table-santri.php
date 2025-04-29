3c05c527b5cfbd444cc4409bd7256739b7beaf17
BangOns <syahr9950@gmail.com>
2025-04-29 22:27:52 +0700

    feat: Create Fitur Search and Report Data Kriteria

--- App/views/data_santri/components/table-santri.php
+++ App/views/data_santri/components/table-santri.php
@@ -1,4 +1,8 @@
 <?php
+$data_santri = $data['data-santri'];
+if (isset($_POST["search"])) {
+    $data_santri = array_filter($data['data-santri'], fn($item) => str_starts_with(strtolower($item['Nama Santri']), strtolower($_POST['search'])));
+};
 $link_menu = [
     [
         'href' => BASEURL . '/data_santri/detail/data-santri',
@@ -27,16 +31,18 @@ $link_menu = [
     <!-- Area Table and Search -->
     <section class="w-full mt-4">
         <!-- Filter and Search -->
-        <header class="w-full flex gap-3  ">
+        <form method="post" class="w-full flex gap-3  ">
             <section class=" lg:w-1/5 rounded flex bg-white px-2 md:px-3 items-center border border-slate-300 py-1">
-                <input type="text" placeholder="Cari Nama Santri"
-                    class="w-full  py-1 border-none bg-transparent text-xs sm:text-sm focus:outline-none focus:ring-0">
+                <input type="text" placeholder="Cari Nama Santri" id="search" name="search"
+                    class=" w-full py-1 border-none bg-transparent text-xs sm:text-sm focus:outline-none focus:ring-0">
                 <div class="size-4 md:size-5 text-slate-600">
                     <?php include dirname(__DIR__, 4) . '/public/icons/icons-search.svg'; ?>
                 </div>
             </section>
-
-        </header>
+            <section class="flex items-center h-full">
+                <button type="submit" class="bg-red-500 text-white px-3 py-2 rounded">Search</button>
+            </section>
+        </form>
         <!-- Button Tambah Pelanggar -->
         <section class="w-full basis-1/2 flex my-4 ">
             <a href="<?= BASEURL; ?>/<?= $data['title']; ?>/add/data-santri"
@@ -50,11 +56,11 @@ $link_menu = [
         <!-- Table & Card Pelanggar -->
         <article class="w-full max-md:space-y-3 max-md:mt-3 ">
             <!-- Table Pelanggar -->
-            <?php if (count($data['data-santri']) !== 0) {
+            <?php if (count($data_santri) !== 0) {
                 include dirname(__DIR__, 3) . '/views/templates/components/table-data.php';
-                renderTable($data['data-santri'], $data['list-table'], $link_menu);
+                renderTable($data_santri, $data['list-table'], $link_menu);
                 include dirname(__DIR__, 3) . '/views/templates/components/card-mobile/card-data-santri.php';
-                renderCardSantri($data['data-santri'], $link_menu);
+                renderCardSantri($data_santri, $link_menu);
             } else {
                 echo '<section class="w-full text-center ">
                 <p class="text-2xl font-semibold">Data Not Found X</p>
@@ -68,10 +74,10 @@ $link_menu = [
 </article>
 <!-- Pagination -->
 
-<section class=" my-3 font-poppins font-semibold ">
-    <ul class="flex w-full justify-center  ">
-        <!-- Cursor Left -->
-        <li class=" border border-slate-300 rounded-l-md ">
+<!-- <section class=" my-3 font-poppins font-semibold "> -->
+<!-- <ul class="flex w-full justify-center  "> -->
+<!-- Cursor Left -->
+<!-- <li class=" border border-slate-300 rounded-l-md ">
             <a href="<?= BASEURL ?>/data_santri?page=<?= $data['halaman-aktif'] - 1 ?>"
                 class="flex w-full h-full <?= $data['halaman-aktif'] <= 1 ? 'pointer-events-none' : ''  ?>   items-center gap-2 text-xs md:text-sm px-3 py-2 hover:cursor-pointer hover:bg-slate-200 bg-white ">
                 <div class="size-4">
@@ -79,19 +85,19 @@ $link_menu = [
                 </div>
                 Previous
             </a>
-        </li>
-        <!-- List Paginate -->
-        <?php for ($i = 1; $i <= $data['total-halaman']; $i++) : ?>
+        </li> -->
+<!-- List Paginate -->
+<!-- <?php for ($i = 1; $i <= $data['total-halaman']; $i++) : ?>
             <li class=" border border-slate-300 ">
                 <a href="<?= BASEURL ?>/data_santri?page=<?= $i ?>"
                     class="flex px-4 items-center h-full  text-xs md:text-sm  w-full hover:cursor-pointer hover:bg-slate-200 bg-white">
                     <?= $i ?>
                 </a>
             </li>
-        <?php endfor; ?>
+        <?php endfor; ?> -->
 
-        <!-- Cursor Right -->
-        <li class=" border border-slate-300 rounded-r-md ">
+<!-- Cursor Right -->
+<!-- <li class=" border border-slate-300 rounded-r-md ">
             <a href="<?= BASEURL ?>/data_santri?page=<?= $data['halaman-aktif'] + 1 ?>"
                 class="flex w-full h-full items-center gap-2 text-xs md:text-sm px-3 py-2 hover:bg-slate-200 bg-white hover:cursor-pointer  <?= $data['halaman-aktif'] >= $data['total-halaman'] ? 'pointer-events-none' : ''  ?>  ">
                 Next
@@ -99,6 +105,6 @@ $link_menu = [
                     <?php include dirname(__DIR__, 4) . '/public/icons/icons-right.svg'; ?>
                 </div>
             </a>
-        </li>
-    </ul>
-</section>
+        </li> -->
+<!-- </ul> -->
+<!-- </section> -->
