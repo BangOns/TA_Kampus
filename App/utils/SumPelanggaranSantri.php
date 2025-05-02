4e6544cba4d19d7f6e22639f5f542415cb738d4a
BangOns <syahr9950@gmail.com>
2025-05-03 00:18:08 +0700

    fix: Fixing Sum

--- App/utils/SumPelanggaranSantri.php
+++ App/utils/SumPelanggaranSantri.php
@@ -5,7 +5,7 @@ function sumPelanggaranSantri($data)
     $c2 = 0.25;
     $c3 = 0.2;
     $c4 = 0.15;
-    $c5 = 0.05;
+    $c5 = 0.15;
 
     $sumNilaiAkhir =  round(($data['jenis'] * $c1) +  ($data['frekuensi'] * $c2) + ($data['dampak'] * $c3) + ($data['keseriusan_niat'] * $c4) + ($data['permohonan_maaf'] * $c5), 2);
 
