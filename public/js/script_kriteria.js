106b92baa3ad2866b2ab4566f929e0c7b1bdbb14
bangOns <syahr9950@gmail.com>
2025-06-02 21:37:55 +0700

    fix: Fixing styling

--- public/js/script_kriteria.js
+++ public/js/script_kriteria.js
@@ -1,3 +1,10 @@
 $(function () {
-  console.log("oke");
+  const showTable = "fixed top-0 left-0 z-10";
+  const modals = $(".modals-kriteria");
+  const ButtonModals = $(".showKriteria");
+  const JudulKriteria = $(".judul-kriteria");
+  ButtonModals.on("click", function () {
+    modals.toggleClass(showTable);
+    JudulKriteria.text("Tambah Kriteria");
+  });
 });
